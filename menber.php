0559326ea44df730228be3154b4d4e2c0bf82edd
junoota0115 <j1u1n5@gmail.com>
2023-12-27 17:52:13 +0900

    社員紹介１

--- menber.php
+++ menber.php
@@ -8,8 +8,33 @@
     <title>社員紹介</title>
 </head>
 <body>
-    <header>
-    </header>
+
+    <div class="header_content_member">
+        <div class="header_lists">
+            <div><img src="./img/icon-member.png" alt="icon"></div>
+            <div>整備について</div>
+            <div>会社概要</div>
+            <div>競技車両政策</div>
+            <div>オンラインショップ</div>
+            <div>お問い合わせ</div>
+        </div>
+    </div>
+
+
+    <div class="member_wrapper1">
+    <div><img src="./img/member-title.png" alt="icon"></div>
+    </div>
+
+    <div class="member_wrapper2">
+        <div class="member_lists">
+            <div class="member_list">
+            <img src="./img/member1.png" >
+            </div>
+            <div class="member_list">
+            <img src="./img/member2.png" >
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
